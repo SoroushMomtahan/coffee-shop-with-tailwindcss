d095d9e985baedf86a2c104b466d2ee97ce320d7
soroush momtahan <soroush_momtahan@outlook.com>
2023-04-30 19:52:30 +0330

    create menu-bar of coffee shop project

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                "DanaRegular": "Dana Regular",
+                "DanaMedium": "Dana Medium",
+                "DanaBold": "Dana Bold",
+                "MorabbaLight": "Morabba Light",
+                "MorabbaMedium": "Morabba Medium",
+                "MorabbaBold": "Morabba Bold",
+            },
+            colors: {
+                "brown": {
+                    "100": "#ECE0D1",
+                    300: "#DBC1AC",
+                    "600": "#967259",
+                    "900": "#634832"
+                }
+            },
+            boxShadow: {
+                "main": "0px 1px 10px rgba(0, 0, 0, 0.05)",
+            },
+            borderRadius: {
+                "rounded":{
+                    "4xl":"2rem"
+                }
+            },
+        },
+    },
+    plugins: [],
+}
+
